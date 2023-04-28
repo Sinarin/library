fc082475039a18fac27fe242a5e48134236dff17
Sinarin <alexngthesis@gmail.com~>
2023-04-28 16:06:45 +0900

    add remove functionality

--- library.js
+++ library.js
@@ -13,10 +13,8 @@ function displayBooks() {
     for (let x = 0; x < mylibrary.length; x++) {
         console.log(`Title:${mylibrary[x].title}, Pages:${mylibrary[x].pages}`);
         const book_form = document.getElementById('body');
-        book_form.innerHTML += `<div>Title:${mylibrary[x].title}, Pages:${mylibrary[x].pages} <button class="remove" data-key="${x}">Remove</button>
-        <button class="read_status" data-key="${x}">Read</button>   </div>
-       
-        ` 
+        book_form.innerHTML += `<div data-key="${x}">Title:${mylibrary[x].title}, Pages:${mylibrary[x].pages} <button class="remove" data-key="${x}">Remove</button>
+        <button class="read_status" data-key="${x}">Read</button>  </div>` 
         }
 }
 
@@ -33,9 +31,10 @@ function buttonAddBook() {
             addBookToLibrary(book);
             alert(`${title.value} has been added to your library`);
             const last = document.getElementById('book');
-            last.insertAdjacentHTML("afterend", `<div>Title:${title.value}, Pages:${pages.value}</div>
-            <button class="remove" data-key="${mylibrary.length}">Remove</button>
-            <button class="read_status" data-key="${mylibrary.length}">Read</button>`)  
+            last.insertAdjacentHTML("afterend", `<div data-key="${mylibrary.indexOf(book)}">Title:${title.value}, Pages:${pages.value}
+            <button class="remove" data-key="${mylibrary.indexOf(book)}">Remove</button>   
+            <button class="read_status" data-key="${mylibrary.indexOf(book)}">Read</button> </div>` );
+            addButtonDeleteBook(mylibrary.indexOf(book));
         }
     })
 }
@@ -44,10 +43,28 @@ function deleteBook(book_number){
     delete mylibrary[book_number]
 }
 
+function addButtonDeleteBook(number){
+    const deleteButton = document.querySelector(`.remove[data-key='${number}'`);
+    deleteButton.addEventListener('click', (e) => {
+        deleteBook(number);
+        const displayedEntry = document.querySelector(`div[data-key="${number}"]`);
+        displayedEntry.remove(); 
+    } )
+}
+
+function startLibraryButtons() {
+    for (let x = 0; x < mylibrary.length; x++){
+        if (mylibrary[x]){
+            addButtonDeleteBook(x);
+        }
+    }
+}
+
 const harry = new Book('Harry Porter', 233);
 addBookToLibrary(harry);
 const harry2 = new Book('Harry Porterrrrr', 233);
 addBookToLibrary(harry2);
 displayBooks();     
 buttonAddBook();
+startLibraryButtons();
 
